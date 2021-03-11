f93fd8ea8509021feeabc78c2a173478925dde26
Pavel Abramchuk <pashaabramchuk92@gmail.com>
2021-03-11 10:38:51 +0200

    complete

--- src/containers/PostPage.js
+++
@@ -1,7 +0,0 @@
-const PostPage = () => {
-    return (
-        <h1>Post page</h1>
-    )
-}
-
-export default PostPage;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MainPage from './containers/MainPage';
+import App from './app';
 
 ReactDOM.render(
-  <>
-    <MainPage />
-  </>,
+    <App />,
   document.getElementById('root')
 );
 
